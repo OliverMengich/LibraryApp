7ac45a95adcad83bd5c2f743cce9416dc5a12ada
Oliver.Mengich <kemeioliver2000@gmail.com>
2021-01-18 09:14:20 +0300

    LibraryApp

---
+++ CustomerSignUp.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+using System.Data.SQLite;
+
+
+// The Content Dialog item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace LibraryApp
+{
+    public sealed partial class CustomerSignUp : ContentDialog
+    {
+        public ComboBoxItem item;
+        public SQLiteConnection SQ;
+        public CustomerSignUp()
+        {
+            this.InitializeComponent();
+
+            SQ = new SQLiteConnection("Data Source=StoreDatabase.sqlite3");
+            if (!File.Exists("./StoreDatabase.sqlite3"))
+            {
+                SQLiteConnection.CreateFile("StoreDatabase.sqlite3");
+            }
+        }
+
+        private void ContentDialog_PrimaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            
+        }
+
+        private void ContentDialog_SecondaryButtonClick(ContentDialog sender, ContentDialogButtonClickEventArgs args)
+        {
+            var ans = item.Content.ToString();
+        }
+
+        private void accountComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var combo = (ComboBox)sender;
+            item = (ComboBoxItem)combo.SelectedItem;
+            var y = item.Content.ToString();
+            if (y== "Administrator")
+            {
+                Selected3.Visibility = Visibility.Visible;
+                Selected2.Visibility = Visibility.Collapsed;
+                Selected1.Visibility = Visibility.Collapsed;
+            }
+            else if (y == "Student")
+            {
+                Selected2.Visibility = Visibility.Visible;
+                Selected1.Visibility = Visibility.Collapsed;
+                Selected3.Visibility = Visibility.Collapsed;
+            }
+            else if (y=="Customer")
+            {
+                Selected2.Visibility = Visibility.Collapsed;
+                Selected3.Visibility = Visibility.Collapsed;
+                Selected1.Visibility = Visibility.Visible;
+            }
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace LibraryApp
+{
+    
+    public sealed partial class MainPage : Page
+    {
+        public ComboBoxItem item;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            
+        }
+        private async void SignUpButton_Click(object sender, RoutedEventArgs e)
+        {
+            CustomerSignUp c =new  CustomerSignUp();
+            await c.ShowAsync();
+        }
+        private void accountComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (ResultTextBlock == null) return;
+            var combo = (ComboBox)sender;
+            item = (ComboBoxItem)combo.SelectedItem;
+            ResultTextBlock.Text = item.Content.ToString();
+        }
+
+        private void loginButton_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+    }
+}
